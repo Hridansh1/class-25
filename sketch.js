f6abb03349213f4c703fe67d5301aa3950db480d
Hridansh1 <hridanshsakaria18@gmail.com>
2021-05-07 14:54:44 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -32,7 +32,8 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+   
+    Platform=new Ground(180,305,300,170)
     bird = new Bird(100,100);
 
 }
@@ -48,7 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+    Platform.display()
     box3.display();
     box4.display();
     pig3.display();
